09064355aa1d37f0b462ce918856a13720c00646
Alexandr <ussov39@gmail.com>
2022-06-06 21:30:49 +0300

    Создана доска объявлений

---
+++ bboard/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
