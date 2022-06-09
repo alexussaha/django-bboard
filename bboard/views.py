5c129e58b6d6f563060c1f305a4db99a0f9e3add
Alexandr <ussov39@gmail.com>
2022-06-09 08:06:08 +0300

    Added Rubric

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rubric
 
 def index(request):
     bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    cerrent_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
