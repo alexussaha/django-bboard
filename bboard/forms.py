526b962854bd85bb6d759f2f25de2d18ac922d70
Alexandr <ussov39@gmail.com>
2022-06-19 18:32:35 +0300

    Finished bboard

---
+++ bboard/forms.py
@@ -0,0 +1,8 @@
+﻿from django.forms import ModelForm
+
+from .models import Bb
+
+class BbForm(ModelForm):
+    class Meta:
+        model = Bb
+        fields = {'title', 'content', 'price', 'rubric'}
